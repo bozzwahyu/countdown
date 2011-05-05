14ce5be3a1f3943fe121c42e2617670120d88720
bozzwahyu <hoezz_teamkoe@yahoo.co.id>
2011-05-06 04:23:28 +0700

    lagi sampai keluar alarm bos

---
+++ countdown/Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.BtnMulai = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtWaktu = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnMulai
+        '
+        Me.BtnMulai.Location = New System.Drawing.Point(65, 66)
+        Me.BtnMulai.Name = "BtnMulai"
+        Me.BtnMulai.Size = New System.Drawing.Size(114, 59)
+        Me.BtnMulai.TabIndex = 1
+        Me.BtnMulai.Text = "Mulai"
+        Me.BtnMulai.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(102, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(13, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "a"
+        '
+        'TxtWaktu
+        '
+        Me.TxtWaktu.Location = New System.Drawing.Point(85, 40)
+        Me.TxtWaktu.Name = "TxtWaktu"
+        Me.TxtWaktu.Size = New System.Drawing.Size(76, 20)
+        Me.TxtWaktu.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 43)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "lama waktu :"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(243, 162)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TxtWaktu)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnMulai)
+        Me.Cursor = System.Windows.Forms.Cursors.Default
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(259, 200)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BtnMulai As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TxtWaktu As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+
+End Class
---
+++ countdown/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    Dim tlsecs As Double
+    Private Sub suara()
+        My.Computer.Audio.Play("D:\belajar vb.net\countdown\Alarm Clock Digital Buzzer.wav")
+    End Sub
+
+    Private Sub tombolAwal()
+        BtnMulai.Enabled = False
+    End Sub
+    Private Sub BtnMulai_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles BtnMulai.Click
+        tlsecs = CDbl(TxtWaktu.Text)
+        Label1.Text = tlsecs.ToString
+        Timer1.Enabled = True
+        tombolAwal()
+    End Sub
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        tlsecs = tlsecs - 1
+        If (tlsecs < tlsecs + 1) Then
+            Label1.ForeColor = Color.Red
+            If (tlsecs <= 0) Then
+                Timer1.Enabled = False
+                BtnMulai.Enabled = True
+                suara()
+            End If
+            Label1.Text = tlsecs.ToString
+        End If
+    End Sub
+
+    
+End Class
